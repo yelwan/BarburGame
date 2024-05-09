5dce3e7d0b6d5b063be8522a53e9de1802f0e70c
ShazaAli <shazamamdouh@aucegypt.edu>
2024-05-09 04:43:20 +0300

    merged code

---
+++ Assets/Scripts/GermsController.cs
@@ -0,0 +1,45 @@
+using UnityEngine;
+using System.Collections;
+
+public class GermsController : MonoBehaviour
+{
+    public GameObject germPrefab;
+    public Transform shootingPosition;
+    private int maxMagnitude = 100;
+    private bool canShoot = true;
+    private bool isSpawning = false;
+
+    void Start()
+    {
+        StartCoroutine(ShootGermsCoroutine());
+    }
+
+    IEnumerator ShootGermsCoroutine()
+    {
+        while (true)
+        {
+            if (canShoot && !isSpawning)
+            {
+                isSpawning = true;
+                yield return new WaitForSeconds(5f);
+                GameObject germ = Instantiate(germPrefab, shootingPosition.position, Quaternion.identity);
+                ApplyRandomContinuousForce(germ);
+                isSpawning = false;
+            }
+
+            yield return null;
+        }
+    }
+
+    private void ApplyRandomContinuousForce(GameObject germ)
+    {
+        Rigidbody2D germRigidbody = germ.GetComponent<Rigidbody2D>();
+
+        if (germRigidbody != null)
+        {
+            germRigidbody.velocity = Random.insideUnitCircle * maxMagnitude / 10f;
+
+            germRigidbody.angularVelocity = Random.Range(-180f, 180f);
+        }
+    }
+}
