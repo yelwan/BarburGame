658c2cb98fddaad0ea71c1fa29c7f1ea51b64986
ShazaAli <shazamamdouh@aucegypt.edu>
2024-05-19 19:42:34 +0300

    final scene

--- Assets/Scripts/GermAnimation.cs
+++ Assets/Scripts/GermAnimation.cs
@@ -4,22 +4,32 @@ using UnityEngine.UI;
 
 public class GermAnimation : MonoBehaviour
 {
-    [SerializeField] Sprite[] AnimSprites;
+    [SerializeField] Sprite[] AnimSpritesG;
+    [SerializeField] Sprite[] AnimSpritesR;
+    [SerializeField] Sprite[] AnimSpritesP;
+    [SerializeField] Sprite[] AnimSpritesO;
+    [SerializeField] Sprite[] AnimSpritesB;
+    Sprite[][] AllAnimSprites;
+
+
     [SerializeField] SpriteRenderer germRenderer;
     public GermFactory factory;
     private Sprite currentSprite;
     private int counter;
+    private int germMag;
     public float switchTime = 0.5f; 
     void OnGUI(){
 }
     void Start()
     {
+         germMag = Random.Range(0, 2);
+        AllAnimSprites = new Sprite[][] { AnimSpritesG, AnimSpritesR, AnimSpritesP, AnimSpritesO, AnimSpritesB };
         counter = 0;
         germRenderer = GetComponent<SpriteRenderer>();
         StartCoroutine("SwitchSprite");
     }
     private IEnumerator SwitchSprite(){
-	currentSprite = AnimSprites[counter++ % AnimSprites.Length];
+	currentSprite = AllAnimSprites[germMag][counter++ % AllAnimSprites[germMag].Length];
     germRenderer.sprite = currentSprite;
 	yield return new WaitForSeconds(switchTime);
 	StartCoroutine("SwitchSprite");
--- Assets/Scripts/GermFactory.cs
+++ Assets/Scripts/GermFactory.cs
@@ -35,3 +35,46 @@ public class GermFactory : MonoBehaviour
     }
     
 }
+/*using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+// the job of this script is to Instantiate germ prefabs and Initialized variables for GermObject script.
+public class GermFactory : MonoBehaviour
+{
+   
+    [SerializeField] GameObject germPrefab;
+    //float randompos;
+    float randomX;
+    Vector3 spawnPosition;
+    [SerializeField] Collider2D spawningAreaCollider;
+    // GermObject objects components
+    [SerializeField] Collider2D GermsCloseness;
+    [SerializeField] Text germcloseText;
+   
+
+    public void createGerm()
+
+    {
+        
+        if (spawningAreaCollider != null)
+        {
+            Bounds colliderBounds = spawningAreaCollider.bounds;
+             randomX = Random.Range(colliderBounds.min.x, colliderBounds.max.x);
+            Vector3 spawnPosition = new Vector3(randomX, colliderBounds.max.y, 0f);
+        }
+
+        if (germPrefab != null)
+        {
+          GameObject germClone =  Instantiate(germPrefab, spawnPosition, Quaternion.identity);
+          GermAnimation germAnim =  germClone.GetComponent<GermAnimation>();
+          germAnim.factory = this;
+          GermObject germObject = germClone.GetComponent<GermObject>();
+          germObject.germcloseText = germcloseText;
+          germObject.spawningArea = GetComponent<GermCorutineManager>().spawningArea;
+          germObject.GermsCloseness = GermsCloseness;
+        }
+    }
+    
+}
+      */
